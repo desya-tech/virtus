3c4621c18b12ff9a23d3b3bafd896ef9b606c17d
desya.kristianto <desya.tech@gmail.com>
2022-05-24 12:47:07 +0700

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 firebase.initializeApp({
  apiKey: "AIzaSyBQ666Cb1GYnpWsfNyTJ8q6ZKkpzxPXCFA",
  authDomain: "virtus-platform-notif.firebaseapp.com",
@@ -9,26 +9,17 @@ firebase.initializeApp({
  appId: "1:275687830048:web:8eb1e2cd03ecdc1d044245",
  measurementId: "G-WBRTH7MNL6"
 });
+console.log("masukkkkkkkkk")
 const messaging = firebase.messaging();
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../firebase-messaging-sw.js')
-      .then(function(registration) {
-        console.log('Registration successful, scope is:', registration.scope);
-      }).catch(function(err) {
-        console.log('Service worker registration failed, error:', err);
-      });
-    }
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
 
-    messaging.setBackgroundMessageHandler(function(payload) {
-      console.log('[firebase-messaging-sw.js] Received background message ', payload);
-      // Customize notification here
-      const notificationTitle = 'Background Message Title';
-      const notificationOptions = {
-        body: 'Background Message body.',
-        icon: './firebase-logo.png'
-      };
-    
-      return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-    });
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
