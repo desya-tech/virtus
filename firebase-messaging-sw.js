c33063ad2d054ca72d40860efb631da450925e03
desya.kristianto <desya.tech@gmail.com>
2022-05-24 12:33:25 +0700

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -19,3 +19,16 @@ if ('serviceWorker' in navigator) {
         console.log('Service worker registration failed, error:', err);
       });
     }
+
+    messaging.setBackgroundMessageHandler(function(payload) {
+      console.log('[firebase-messaging-sw.js] Received background message ', payload);
+      // Customize notification here
+      const notificationTitle = 'Background Message Title';
+      const notificationOptions = {
+        body: 'Background Message body.',
+        icon: './firebase-logo.png'
+      };
+    
+      return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+    });
