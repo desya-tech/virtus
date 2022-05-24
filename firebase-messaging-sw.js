4c8a3b4701867a7416b1ce252a5249ae62cee4ee
desya.kristianto <desya.tech@gmail.com>
2022-05-24 12:54:20 +0700

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 firebase.initializeApp({
  apiKey: "AIzaSyBQ666Cb1GYnpWsfNyTJ8q6ZKkpzxPXCFA",
  authDomain: "virtus-platform-notif.firebaseapp.com",
@@ -9,17 +9,4 @@ firebase.initializeApp({
  appId: "1:275687830048:web:8eb1e2cd03ecdc1d044245",
  measurementId: "G-WBRTH7MNL6"
 });
-console.log("masukkkkkkkkk")
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
